8c02a6e0a80315ce3976a1491161fb4b817650ea
Mrhuangchina <jianmissyoulove@qq.com>
2017-07-26 21:53:31 +0800

    Initial Commit

---
+++ HJPageView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HJPageView
+//
+//  Created by MrHuang on 17/7/26.
+//  Copyright © 2017年 Mrhuang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
